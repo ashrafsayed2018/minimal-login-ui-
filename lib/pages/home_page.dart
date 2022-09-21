56868b1a0edf99bdd08d16fede291c52c235a5ef
ashrafsayed2018 <ashrafsayed19841111@gmail.com>
2022-09-21 14:37:26 +0300

    read and display data

--- lib/pages/home_page.dart
+++ lib/pages/home_page.dart
@@ -1,33 +1,83 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
 import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter/gestures.dart';
 import 'package:flutter/material.dart';
+import 'package:minimal_login_ui/constants/colors.dart';
+import 'package:minimal_login_ui/read_data/get_user_name.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({Key? key}) : super(key: key);
 
   @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> {
+  final User? user = FirebaseAuth.instance.currentUser;
+
+  // list of docs id
+
+  List<String> docIds = [];
+  // get the docs ids
+
+  Future getDocsIds() async {
+    await FirebaseFirestore.instance.collection("users").get().then(
+      (snapshot) {
+        for (var document in snapshot.docs) {
+          docIds.add(document.reference.id);
+        }
+      },
+    );
+  }
+
+  @override
+  void initState() {
+    super.initState();
+  }
+
+  @override
   Widget build(BuildContext context) {
-    final User? user = FirebaseAuth.instance.currentUser;
     return Scaffold(
+      appBar: AppBar(
+        backgroundColor: AppColor.kPrimaryColor,
+        elevation: 0,
+        title: Text(
+          user!.email!,
+          style: const TextStyle(fontSize: 16, fontWeight: FontWeight.bold),
+        ),
+        actions: [
+          GestureDetector(
+            onTap: () {
+              FirebaseAuth.instance.signOut();
+            },
+            child: const Icon(Icons.logout),
+          )
+        ],
+      ),
       body: Center(
         child: Column(
           mainAxisAlignment: MainAxisAlignment.center,
           crossAxisAlignment: CrossAxisAlignment.center,
           children: [
-            Text(
-              user!.email!,
-              style: const TextStyle(fontSize: 20, fontWeight: FontWeight.bold),
+            Expanded(
+              child: FutureBuilder(
+                future: getDocsIds(),
+                builder: (BuildContext context, AsyncSnapshot snapshot) {
+                  return ListView.builder(
+                    itemCount: docIds.length,
+                    itemBuilder: (BuildContext context, int index) {
+                      return Padding(
+                        padding: const EdgeInsets.all(8.0),
+                        child: ListTile(
+                          tileColor: AppColor.kLightGreyColor,
+                          title: GetUserName(documentId: docIds[index]),
+                        ),
+                      );
+                    },
+                  );
+                },
+              ),
             ),
-            const SizedBox(
-              height: 20,
-            ),
-            MaterialButton(
-              color: Colors.deepPurple.shade600,
-              textColor: Colors.white,
-              onPressed: () {
-                FirebaseAuth.instance.signOut();
-              },
-              child: const Text("Sign Out"),
-            )
           ],
         ),
       ),
---
+++ lib/read_data/get_user_name.dart
@@ -0,0 +1,25 @@
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:flutter/material.dart';
+
+class GetUserName extends StatelessWidget {
+  final String documentId;
+  const GetUserName({required this.documentId, Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    // get the collection
+    CollectionReference users = FirebaseFirestore.instance.collection("users");
+    return FutureBuilder<DocumentSnapshot>(
+      future: users.doc(documentId).get(),
+      builder: (BuildContext context, AsyncSnapshot snapshot) {
+        if (snapshot.connectionState == ConnectionState.done) {
+          Map<String, dynamic> data =
+              snapshot.data!.data() as Map<String, dynamic>;
+
+          return Text("first name ${data['first_name']}");
+        }
+        return const Text("loading ....");
+      },
+    );
+  }
+}
